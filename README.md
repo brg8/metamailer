2fd9960ce11a8008f8dd0680d9b6ea8c4836ed1b
Benjamin Godlove <bgodlove88@gmail.com>
2014-04-07 23:11:58 -0700

    Made readme clearer

--- README.md
+++ README.md
@@ -3,5 +3,13 @@ Metamailer is a wrapper for sending email through a couple providers. Right now 
 ## Example Usage
 ```
 mailgun = Mailgun.new("username", "api_key")
+# be sure to format your from address correctly
+from = "Benjamin Godlove <benjamin.godlove@test.com>"
+to = "test@test.com"
+subject = html = text = "test"
 mailgun.send(from, to, subject, html, text)
 ```
+
+#### PostageApp
+
+You can only have one postageapp account, unfortunately.
--- lib/postageappm.rb
+++ lib/postageappm.rb
@@ -1,11 +1,17 @@
 gem 'postageapp', '=1.0.23'
 require 'postageapp'
 
-PostageApp.configure do |config|
-  config.api_key = ""# api key goes here
-end
+#PostageApp.configure do |config|
+#  config.api_key = ""# api key goes here
+#end
 
 class PostageAppMailer < MetaMailer
+  def initialize(api_key)
+    PostageApp.configure do |config|
+      config.api_key = ""
+    end
+  end
+
   def send(from, to, subject, html, text)
     request = PostageApp::Request.new(:send_message, {
       'headers' => {
